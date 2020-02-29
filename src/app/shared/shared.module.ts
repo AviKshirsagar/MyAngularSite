d95d1aef8b2733d19f657a814f37bc8afec76ba1
Avinash <avikshirsagar4@gmail.com>
2020-02-29 18:51:17 +0530

    Till now its working fine. locations of sidebar, header, footer all are set exactly where they needs to be at.
    1. Two background Images are uploaded in the assets.
    2. added rights reserved and some mat-icon buttons in the footer.html .
    3. Under default css file we added the styles for mat-drawer and containers. :host{
        some display : flex code.
    }
    4. styles for footer also.
    5.Mat-iconModule, ButtonModule and material related modules are imported in shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,9 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -11,7 +14,10 @@ import { CommonModule } from '@angular/common';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule
   ],
   exports : [
     HeaderComponent,
