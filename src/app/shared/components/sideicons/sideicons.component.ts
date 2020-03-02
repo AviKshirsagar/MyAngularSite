c6604cfea757de520ceba08c216c7aacc327dc77
Avinash <avikshirsagar4@gmail.com>
2020-03-02 18:45:15 +0530

    Component Added: 1. New sideicon component is added for icons at the left side, and added some material icons into it. 2. Some style added and the flex-coding written for it in order to make it more responsive.

---
+++ src/app/shared/components/sideicons/sideicons.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-sideicons',
+  templateUrl: './sideicons.component.html',
+  styleUrls: ['./sideicons.component.css']
+})
+export class SideiconsComponent implements OnInit {
+  
+  constructor(){}
+
+  ngOnInit(): void {
+  }
+
+}
