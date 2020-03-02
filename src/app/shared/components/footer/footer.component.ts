fac72a7494d7b720a7227453e2910d231dc67917
Avinash <avikshirsagar4@gmail.com>
2020-03-02 23:47:52 +0530

    Implementation of custom Icons from ico file to svg 1. Under app.module.ts file imported HttpClientModule and in the Imports: [ ] array too.
    2. Under footer.component.ts file imported MatIconRegistry from material/icons, and DomSanitizer from platformBrowser.
    
    In the same file impelemented addSvgIcon() method under constructor.
    
    In the same one Implemented onClick() function for navigation on click event of Icons in the footer.
    
    3. Some flex-layout (fxLayout) scripting is implemented in footer html file.
    
    4. Css and hover effects scripting style is implemented under footer html.

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
+import { MatIconRegistry } from '@angular/material/icon';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-footer',
@@ -7,8 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  // linkGithub = "https://github.com/AviKshirsagar";
 
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      "angular",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("assets/Icons/svg/angular2.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "git",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("assets/Icons/svg/Github.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "chrome",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("assets/Icons/svg/downloadBackup.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "gmail",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("assets/Icons/svg/G-MAIL.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "insta",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("assets/Icons/svg/INSTA.svg")
+    );{
+  };
+}
+    onClick(){
+      window.open('https://github.com/AviKshirsagar');
+  }
   ngOnInit(): void {
   }
 
